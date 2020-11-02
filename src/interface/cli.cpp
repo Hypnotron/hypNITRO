40772ae29d07fd73adbd0a12dce2be3f8d485d25
Hypnotron <the.hypnotron@gmail.com>
2020-11-02 14:29:40 -0500

    parent d4a51569bab8a2dbc4472cdf215ebbbffb44edaf author Hypnotron <the.hypnotron@gmail.com> 1604345380 -0500 committer Hypnotron <the.hypnotron@gmail.com> 1604469559 -0500
    CMakeLists fallback for missing SDL2/Vulkan
    
    Fix for experimental GitHub Actions
    
    Fix for experimental GitHub Actions
    
    Fix for experimental GitHub Actions
    
    Fix for experimental GitHub Actions
    
    Fix for experimental GitHub Actions
    
    Fix for experimental GitHub Actions
    
    Fix for experimental GitHub Actions
    
    Fix for experimentla GitHub Actions
    
    Fix for experimental GitHub Actions
    
    Fix for experimental (spelled correctly this time) GitHub Actions
    
    Fix for experimental GitHub Actions
    
    Fix for experimental GitHub Actions
    
    Fix for experimental GitHub Actions
    
    Fix for experimental GitHub Actions
    
    Fix for experimental GitHub Actions
    
    Fixed typo in GitHub workflow
    
    Fixes for experimental GitHub Actions
    
    fix github actions
    
    fixes for GitHub Actions
    
    fix github actions
    
    debug github actions
    
    debug github actions
    
    debug github actions
    
    remove github actions debugging
    
    fixes for github actions
    
    Revert to original CMakeLists.txt
    
    Switch to pkg-config for Vulkan and SDL2 in CMakeLists.txt
    
    fixes for github actions
    
    testing github actions
    
    testing github actions
    
    testing github actions
    
    testing github actions
    
    testing github actions
    
    testing github actions
    
    testing github actions
    
    testing github actions
    
    testing github actions
    
    testing github actions
    
    Working Ubuntu + macOS CI! Still a work in progress
    
    testing github actions
    
    testing github actions
    
    testing github actions
    
    testing github actions
    
    testing github actions
    
    testing github actions
    
    testing github actions
    
    testing github actions
    
    testing github actions
    
    Guys, each job runs in a separate virtual machine even if the OS is the same, shame on me for taking all day to figure this out
    Anyway, fixed GitHub Actions
    
    I forgot an equals sign, darn it
    
    I promise this is (one of) the last GitHub Actions commits
    
    Returned to original CMakeLists.txt
    
    Testing GitHub Actions
    
    Fix CMakeLists.txt invalid PkgConfig usage
    
    Trying pkg-config style CMakeLists.txt again
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Disabling Windows GitHub Actions for now
    
    Testing GitHub Actions
    
    Hybird PkgConfig/FindPackage approach
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Testing GitHub Actions
    
    Almost done testing GitHub Actions
    
    GitHub Actions allllmmooooooost finished I promise
    
    Final GitHub Actions commit
    
    Experimental artifacts for GitHub Actions
    
    Experimental artifacts for GitHub Actions
    
    SDL2 as a submodule
    
    Revert "Experimental artifacts for GitHub Actions"
    
    This reverts commit b0c82033d0bde0f0ec046db57b673287badb3dc2.
    
    Testing GitHub Actions artifacts
    
    Testing GitHub Actions artifacts
    
    Testing GitHub Actions artifacts
    
    Testing GitHub Actions artifacts
    
    Testing GitHub Actions artifacts
    
    Testing GitHub Actions artifacts
    
    Switch to meson, I'm sick of CMake
    
    Testing Meson CI
    
    Testing Meson CI
    
    Testing Meson CI
    
    Testing Meson CI
    
    Testing Meson CI
    
    Testing Meson CI
    
    Testing Meson CI
    
    Testing Meson CI
    
    Testing Meson CI
    
    Testing Meson CI
    
    Testing Meson CI
    
    Working Ubuntu and macOS CI!
    
    testing github actions
    
    testing github actions

--- src/interface/cli.cpp
+++ src/interface/cli.cpp
@@ -1,3 +1,27 @@
+#include <SDL.h>
+
 int main(int argc, char* argv[]) {
+    SDL_Log("hypnitro version 0.0.0\n");
+
+    if (SDL_Init(SDL_INIT_EVERYTHING)) {
+        SDL_Log("Cannot initialize SDL2! Error: %s\n", SDL_GetError());
+        return 1;
+    }
+    SDL_Window* window = SDL_CreateWindow(
+            "hypNITRO", /* title */
+            SDL_WINDOWPOS_UNDEFINED, /* x */
+            SDL_WINDOWPOS_UNDEFINED, /* y */
+            640, /* width */
+            480, /* height */
+            SDL_WINDOW_VULKAN); /* flags */
+    if (!window) {
+        SDL_Log("Cannot create window! Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Delay(3000);
+        
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
--- src/interface/test.cpp
+++ src/interface/test.cpp
@@ -1,5 +1,3 @@
-#include <SDL.h>
-
 int main(int argc, char* argv[]) {
-    return SDL_RWFromFile("test.nds", "rb") ? 0 : 1;
+    return 0; 
 }
